4023e97f4fa42598d8b71914d3283242a5ce91e2
marijadragicevic <majadragicevic.111@gmail.com>
2022-05-10 00:26:27 +0200

    adding style

---
+++ calculator/src/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex: 50%;
+
+    button {
+        border:none;
+        padding:5px 10px;
+        margin:5px;
+        font-size:1.2rem;
+    }
+`
